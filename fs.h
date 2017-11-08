019814c017c9d9aa93b242848510434d03b90ff0
Marcel <mk@freeshell.de>
2017-11-09 00:35:58 +0100

    Add mntcmd_u member for user mode mount command.

--- fs.h
+++ fs.h
@@ -36,9 +36,10 @@ typedef enum FSID {
 typedef struct fs_s {
 	const char *name;
 	FSID	   id;
-	const char *mntcmd; /* User defined mount command */
-	const char *dopts;  /* Default mount options. */
-	const char *uopts;  /* User defined mount options. */
+	const char *mntcmd;   /* User defined mount command */
+	const char *mntcmd_u; /* User defined mount command for user mode */
+	const char *dopts;    /* Default mount options. */
+	const char *uopts;    /* User defined mount options. */
 } fs_t;
 
 extern fs_t fstype[];
